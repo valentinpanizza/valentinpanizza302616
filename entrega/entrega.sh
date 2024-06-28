de94d38c6c2c3e12294954e6d6937364c2d7907f
valentinpanizza <valentinpanizza21@gmail.com>
2024-06-27 21:14:48 -0300

    Entrega de Valentin Panizza - 302616

--- entrega/entrega.sh
+++ entrega/entrega.sh
@@ -1,2 +1,57 @@
-# Entrega Nombre Apellido - NumeroDeEstudiante
+# Entrega Valentin Panizza - 302616
 
+while true; do
+
+	clear
+	echo "## COPA AMÉRICA 2024 ##"
+	echo " "
+	echo "1) ¿Qué países participan?"
+	echo "2) ¿Cuáles ya la han ganado?"
+	echo "3) ¿Qué país va a ganar esta?"
+	echo "0) Salir"
+	echo " "
+	read -p "Ingrese una opción: " op
+	clear
+
+	case $op in
+		1)
+			echo "## LISTA DE PARTICIPANTES ##"
+			echo ""
+			echo "Grupo A. Argentina Perú Chile Canadá"
+			echo "Grupo B. México Ecuador Venezuela Jamaica"
+			echo "Grupo C. Estados Unidos Uruguay Panamá Bolivia"
+			echo "Grupo D. Brasil Colombia Paraguay Costa Rica"
+			echo ""
+			read -n 1 -s -p "Presiona cualquier tecla para volver al menú: "
+                        read -t 0.1 -n 10000
+		;;
+		2)
+			echo "## LISTA DE GANADORES HISTÓRICOS ##"
+			echo ""
+                        echo "15: Uruguay y Argentina"
+			echo "9: Brasil"
+			echo "2: Perú, Paraguay y Chile"
+			echo "1: Bolivia y Colombia"
+                        echo ""
+			read -n 1 -s -p "Presiona cualquier tecla para volver al menú: "
+                        read -t 0.1 -n 10000
+		;;
+		3)
+			echo "## PREDICCIONES ##"
+			echo ""
+			read -p "Ingrese el país que cree que va a ganar: " pre
+			sleep 2
+			echo "Su predicción: $pre"
+			echo "Mi predicciñon: Uruguay"
+			sleep 2
+		;;
+		0)
+			echo "Saliendo..."
+			exit
+		;;
+		*)
+			echo "Ingrese una opción válida..."
+			sleep 1
+		;;
+	esac
+done
